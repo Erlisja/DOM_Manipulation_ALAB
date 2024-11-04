3f9d0352290ad807ad3d6cbddf876053e7b60a0b
Erlisja Kore <korelisa@yahoo.com>
2024-11-03 23:04:55 -0500

    Finalized lab

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// Menu data structure
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+  ];
+
+
+// Part 1: Getting Started
+const mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
+mainEl.classList.add("flex-ctr");
+
+// Part 2: Creating a Menu Bar
+const topMenuEl = document.querySelector("#top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
+// Part 3: Adding Menu Buttons
+menuLinks.forEach((link) => {
+    const newElement = document.createElement("a");
+    newElement.setAttribute("href", link.href);
+    newElement.textContent = link.text;
+    topMenuEl.appendChild(newElement);
+});
